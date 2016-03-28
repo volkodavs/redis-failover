cc56ed7c3a00371f896dbdb97d531300a8107d3a
Sergey Volokodav <svolkodav@xanadu.ie>
2016-03-28 20:00:49 +0100

    Monitor wait until redis up

--- docker-redis/monitor/docker-entrypoint.sh
+++ docker-redis/monitor/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+while ! nc -q 1 localhost 6379 </dev/null; do sleep 1; done
+while ! nc -q 1 localhost 6380 </dev/null; do sleep 1; done
+while ! nc -q 1 localhost 6381 </dev/null; do sleep 1; done
 
 redis-stat localhost:6379 localhost:6380 localhost:6381 --server=8080 5
